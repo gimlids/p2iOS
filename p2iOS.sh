ed9df1a11b76c5fdb382cbf2a868859f2f8bf703
David Stolarsky <david.stolarsky@gmail.com>
2012-05-03 01:01:59 -0400

    format C++ with astyle

--- p2iOS.sh
+++ p2iOS.sh
@@ -4,9 +4,12 @@
 
 js=`which js`
 
+output_path="runtime/openFrameworks/apps/p2iOSapps/p2iOSApp/src/main.mm"
+
 #if [[ "$uname" == "Linux" -o "$uname" == "Darwin" ]]
 #then
-   ./tools/pde2cpp.py $js $1 > runtime/openFrameworks/apps/p2iOSapps/p2iOSApp/src/main.mm
+   ./tools/pde2cpp.py $js $1 > $output_path
+   astyle $output_path
 #else
 #   ./tools/pde2cpp.py ../jsshell/js $1
 #fi
